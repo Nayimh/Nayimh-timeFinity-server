f1e9a31e23e8b7fb9176bf15ccc047ceedcc1dd0
Nayimh <hnaim6979@gmail.com>
2022-01-02 06:50:49 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const app = express();
+const { MongoClient } = require('mongodb');
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+require('dotenv').config();
+
+// midleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cetyr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('timeFinity');
+        const watchCollection = database.collection('watch');
+
+        // post data from ui to db
+        app.post('/watch', async (req, res) => {
+            const data = req.body;
+            const result = await watchCollection.insertOne(data);
+            res.json(result);
+        })
+
+        // get data from db to ui
+        app.get('/watch', async (req, res) => {
+            const data = watchCollection.find({});
+            const result = data.toArray();
+            res.json(result);
+        })
+
+    }
+
+    finally {
+        // await client.close();
+    }
+    
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('group task server is running');
+})
+
+app.listen(port, () => {
+    console.log('listning to port', port);
+})
+
